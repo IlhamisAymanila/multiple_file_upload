0d2ca0bf44db9875d29d340cb8650f2c9765423a
IlhamisAymanila <74014773+IlhamisAymanila@users.noreply.github.com>
2022-09-04 20:26:42 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+import Test from "./Test";
+import Test2 from "./Test2";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <Test2 />
+    </div>
+  );
+}
